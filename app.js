c484afd37f2b3ca7667c72007200ef390791d535
nodexperts <dev@nodexperts.com>
2019-04-17 22:18:52 +0530

    Completed notes app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('Starting app.js');
+
 const fs = require('fs');
 const _ = require('lodash');
 const notes = require('./notes.js');
@@ -6,30 +6,34 @@ const yargs = require('yargs');
 
 const { argv } = yargs;
 
-console.log('args', argv,);
 const command = argv._[0];
 
 switch(command){
     case 'add': {
         const { title, body } = argv;
-        notes.addNote(title, body);
+        const note = notes.addNote(title, body);
+        if(note) {
+            console.log('Note created successfully!')
+            notes.logNote(note);
+        }
+        else console.log('Note title already exist!')
         break;
     }
     case 'list': {
-        console.log('List Note');
         notes.getAll();
         break;
     }
     case 'read': {
         console.log('Read Note');
-        const { title } = argv;
-        notes.getNote(title);
+        const note = notes.getNote(argv.title);
+        if(note)notes.logNote(note);
+        else console.log('Note not found')
         break;
     }
     case 'remove': {
-        console.log('Remove Note');
-        const { title } = argv;
-        notes.removeNote(title);
+        const noteRemoved = notes.removeNote(argv.title);
+        let message = noteRemoved ? 'Note removed successfully': 'Note not found';
+        console.log(message)
         break;
     }
 }
--- notes.js
+++ notes.js
@@ -1,24 +1,60 @@
-console.log('Starting note.js');
+const fs = require('fs');
+let fetchNotes = () => {
+    try {
+        let notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+        return notes;
+    }catch(error){
+        return [];
+    }
+}
 
+let saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
 let addNote = (title, body) => {
-    console.log('title:', title, 'body:', body)
+    let notes = fetchNotes();
+    let note = { title, body };
+    let duplicateNotes = notes.filter(item => item.title === title)
+    if(!duplicateNotes.length) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+    // return null;
 };
 
 let getAll = () => {
-    console.log('getAll')
+    const notes = fetchNotes();
+    console.log(`Printing ${notes.length} notes`);
+    notes.forEach((note, index)=>{
+        console.log(`\nNote ${index+1}`);
+        logNote(note);
+    })
 }
 
 let getNote = (title) => {
-    console.log('get Note', title)
+    const notes = fetchNotes();
+    return notes.find(note => note.title == title);
 }
 
 let removeNote = (title) => {
-    console.log('Remove Note', title)
+    const notes = fetchNotes();
+    const filteredNotes = notes.filter(note => note.title !== title);
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;  
+}
+
+let logNote = (note) => {
+    console.log('-----------------');
+    console.log('Title: ', note.title);
+    console.log('Body: ', note.body);
 }
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 };
